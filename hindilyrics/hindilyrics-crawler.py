0d44a3a0743f97438a21a872bd19196bbd97f705
Pratyush Singh <singh.pratyush96@gmail.com>
2016-05-06 01:00:22 +0530

    Added nested logging support and removed improper folder existence checking method.

--- hindilyrics/hindilyrics-crawler.py
+++ hindilyrics/hindilyrics-crawler.py
@@ -2,7 +2,7 @@ import math
 import string
 import sys
 from datetime import datetime
-from os import _exists, mkdir
+from os import path, mkdir
 from threading import Thread
 from time import time
 from urllib import request
@@ -11,8 +11,10 @@ usage = "python hindilyrics-crawler <crawl type - full/incr> <destination>"
 start_address = 'http://www.hindilyrics.net'
 
 
-def print_info(message):
+def print_info(level, message):
     message = '(' + datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S') + ') INFO: ' + message
+    for i in range(0, level):
+        message = '\t' + message
     print(message)
 
 
@@ -20,38 +22,45 @@ def download_movie(url):
     pass
 
 
-def download_movies_from_page(init, number):
+def download_movies_from_page(level, init, number):
     global start_address
-    print_info('Started thread for downloading movies starting with {0} page {1}'.format(init, number))
+    print_info(level, 'Started thread for downloading movies starting with {0} page {1}'.format(init, number))
+
+    website = start_address + '/lyrics/hindi-songs-starting-{0}-page-{1}.html'.format(init, number)
+    raw_html = str(request.urlopen(website).read())
+
+
+
     pass
 
 
-def initial(init):
+def initial(level, init):
     global start_address
-    print_info('Started thread for downloading metadata for movies starting with {0}'.format(init))
+    print_info(level, 'Started thread for downloading metadata for movies starting with {0}'.format(init))
     website = start_address + '/lyrics/hindi-songs-starting-{0}-page-1.html'.format(init)
 
     raw_html = str(request.urlopen(website).read())
     number_of_movies = raw_html.count('<li>')
 
-    print_info('Found {0} movies starting with {1}.'.format(number_of_movies, init))
+    print_info(level, 'Found {0} movies starting with {1}.'.format(number_of_movies, init))
 
     number_of_pages = math.ceil(number_of_movies / 90.0)
 
     threads = {}
     for i in range(1, number_of_pages + 1):
-        threads[i] = Thread(target=download_movies_from_page, args=(init, i))
+        threads[i] = Thread(target=download_movies_from_page, args=(level + 1, init, i))
         threads[i].start()
 
 
-def full_crawl():
+def full_crawl(level=0):
     initials = ['0'] + string.ascii_lowercase
 
     while True:
+        print_info(level, 'Starting full crawl')
         thread_dict = {}
 
         for init in initials:
-            thread_dict[init] = Thread(target=initial, args=(init,))
+            thread_dict[init] = Thread(target=initial, args=(level + 1, init))
             thread_dict[init].start()
 
         for init in initials:
@@ -72,11 +81,11 @@ def main():
     if crawl_type not in ('full', 'incr'):
         raise ValueError('Invalid crawl type')
 
-    if not _exists(sys.argv[2]):
+    if not path.isdir(sys.argv[2]):
         mkdir(sys.argv[2])
 
     if crawl_type.matches('full'):
-        full_crawl()
+        full_crawl(0)
     else:
         incremental_crawl()
 
