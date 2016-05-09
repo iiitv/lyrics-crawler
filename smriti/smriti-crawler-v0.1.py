f67819b7203ac2339a05ecfaf3257c5d60cc523f
Pratyush Singh <singh.pratyush96@gmail.com>
2016-05-09 10:38:34 +0530

    smriti.com v0.1 : Added additional header to avoid HTTP 403 error

--- smriti/smriti-crawler-v0.1.py
+++ smriti/smriti-crawler-v0.1.py
@@ -1,16 +1,18 @@
 from queue import Queue
 from re import findall, DOTALL
+from string import ascii_lowercase
+from threading import Thread
 from urllib import request
 
 from print_util import print_error, print_info
 
 task_queue = Queue()
 location = 'downloads/'
-start_address = 'http://www.smriti.com'
+start_address = 'http://smriti.com'
 
 
 def download_movie(thread_id, init, url, movie):
-    pass
+    return
 
 
 def get_movies(thread_id, init):
@@ -18,15 +20,18 @@ def get_movies(thread_id, init):
 
     print_info('{0} : Getting webpage for movies starting with \'{'
                '1}\''.format(thread_id, init))
-    website = start_address + '/hindi-sings/movie-{0}'.format(init)
+    website = start_address + '/hindi-songs/movies-{0}'.format(init)
     done = False
     raw_html = ''
+    hdr = {'User-Agent': 'Mozilla/5.0'}
     while not done:
         try:
-            raw_html = str(request.urlopen(website).open())
+            req = request.Request(website, headers=hdr)
+            raw_html = str(request.urlopen(req).read())
             done = True
-        except Exception:
-            print_error('{0} : Error occurred, retrying')
+        except Exception as e:
+            print_error('{0} : Error occurred while getting {1}, '
+                        'retrying'.format(thread_id, website))
 
     main_content = findall(r'<a href=\"/hindi-songs/\">main index</a>('
                            r'.*?)</div>', raw_html, DOTALL)[0]
@@ -47,9 +52,21 @@ def movie_threader(thread_id):
             download_movie(thread_id, task[1], task[2], task[3])
 
 
-def main():
-    return
+def main(number_of_threads):
+    thread_dict = {}
+    for i in range(1, number_of_threads + 1):
+        temp = Thread(target=movie_threader, args=(i,))
+        temp.start()
+        thread_dict[i] = temp
+
+    crawl_list = ['1', ] + list(ascii_lowercase)
+    crawl_list.remove('x')
+    for i in crawl_list:
+        task_queue.put((0, i))
+
+    for i in range(1, number_of_threads + 1):
+        thread_dict[i].join()
 
 
 if __name__ == "__main__":
-    main()
+    main(4)
