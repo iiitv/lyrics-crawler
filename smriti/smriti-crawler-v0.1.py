7adced978ba52c7457d58977d56009cd3be7c0f4
Pratyush Singh <singh.pratyush96@gmail.com>
2016-05-09 23:33:27 +0530

    smriti.com v0.1 : Completed function to get all movies starting with particular character.

--- smriti/smriti-crawler-v0.1.py
+++ smriti/smriti-crawler-v0.1.py
@@ -9,21 +9,48 @@ from print_util import print_error, print_info
 task_queue = Queue()
 location = 'downloads/'
 start_address = 'http://smriti.com'
+hdr = {'User-Agent': 'Mozilla/5.0'}
 
 
 def download_movie(thread_id, init, url, movie):
+    global location, start_address, hdr
+
+    print_info('{0} : Getting webpage for movie {1} - {2}'.format(thread_id,
+                                                                  movie, url))
+
+    raw_html = ''
+    website = start_address + url
+    done = False
+    while not done:
+        try:
+            req = request.Request(website, headers=hdr)
+            raw_html = str(request.urlopen(req).read())
+            done = True
+        except Exception:
+            print_error('{0} : Error occurred while getting {1}, '
+                        'retrying'.format(thread_id, website))
+
+    main_content = findall(r'index</a><br/>\n(.*?)</div> <', raw_html, DOTALL)[
+        0]
+
+    song_with_url = findall(r'<div class="onesong">(.*?): <a href=.*?<a '
+                            r'href="(.*?)">', main_content, DOTALL)
+
+    print(song_with_url)
+
+    file_path = location + init + '/' + movie + '.json'
+    print_info('{0} : Saving movie data to {1}'.format(thread_id, file_path))
     return
 
 
 def get_movies(thread_id, init):
-    global start_address
+    global start_address, hdr
 
     print_info('{0} : Getting webpage for movies starting with \'{'
                '1}\''.format(thread_id, init))
     website = start_address + '/hindi-songs/movies-{0}'.format(init)
     done = False
     raw_html = ''
-    hdr = {'User-Agent': 'Mozilla/5.0'}
     while not done:
         try:
             req = request.Request(website, headers=hdr)
@@ -36,6 +63,11 @@ def get_movies(thread_id, init):
     main_content = findall(r'<a href=\"/hindi-songs/\">main index</a>('
                            r'.*?)</div>', raw_html, DOTALL)[0]
     movies_with_url = findall(r'<a href=\"(.*?)\">(.*?)</a>', main_content)
+    print_info(
+        '{0} : Found {1} movies starting with {2}'.format(
+            thread_id, len(movies_with_url), init
+        )
+    )
     for url, movie in movies_with_url:
         init = '0' if init == '1' else init
         movie = movie.replace('/', '_').replace(':', '_').replace('-', '_')
@@ -46,6 +78,7 @@ def movie_threader(thread_id):
     while True:
         task = task_queue.get()
 
+        print_info('{0} : New task - {1}'.format(thread_id, task))
         if task[0] == 0:
             get_movies(thread_id, task[1])
         elif task[0] == 1:
