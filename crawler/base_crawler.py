e0aa703637605800781d64e987f20a4ef87320b5
Pratyush Singh <singh.pratyush96@gmail.com>
2016-06-18 20:24:54 +0530

    Improved CrawlerType0 and printing process.

--- crawler/base_crawler.py
+++ crawler/base_crawler.py
@@ -6,6 +6,7 @@ from urllib import request
 
 import db_operations
 import print_util
+from print_util import colors
 
 headers = [
     'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
@@ -31,136 +32,218 @@ headers = [
 
 
 class BaseCrawler:
-    def __init__(self, name, start_url):
+    def __init__(self, name, start_url, number_of_threads, max_err=10,
+                 delay_request=False):
+        """
+
+        :param name: Name of crawler
+        :param start_url: Base URL of website
+        :param number_of_threads: Number of threads to use to crawl
+        :param max_err: Max number of allowed errors for a crawl
+        :param delay_request: Whether to delay while making requests or not
+        """
+        self.delay_request = delay_request
         self.name = name
         self.start_url = start_url
-        db_operations.create()
+        self.number_of_threads = number_of_threads
+        self.max_allowed_errors = max_err
 
 
 class CrawlerType0(BaseCrawler):
-    def __init__(self, name, start_url, list_of_url, number_of_threads):
-        super().__init__(name, start_url)
+    def __init__(self, name, start_url, list_of_url, number_of_threads,
+                 max_err=10, delay_request=False):
+
+        # Constructor for BaseCrawler
+        super().__init__(name, start_url, number_of_threads, max_err,
+                         delay_request)
+
+        # Initialize data members
+        self.task_queue = LifoQueue()
         self.url_list = list_of_url
-        self.number_of_threads = number_of_threads
-        self.task_queue = Queue()
 
     def threader(self, thread_id):
-        while not self.task_queue.empty():
-            task = self.task_queue.get()
+        while not self.task_queue.empty():  # While there are any tasks
 
-            if task[0] == 0:
-                self.get_movies(thread_id, task[1])
-            elif task[0] == 1:
-                self.download_movie(thread_id, task[1], task[2])
+            task = self.task_queue.get()  # Get one of them
+
+            if task is None:  # Queue has been emptied, abort
+                return
+
+            if task['n_errors'] >= self.max_allowed_errors:  # Too many errors
+                print_util.print_warning(
+                    '{0} --> Too many errors in task {1}. Skipping.'.format(
+                        thread_id,
+                        task
+                    )
+                )
+                continue
+
+            print_util.print_info(
+                '{0} --> New task : {1}'.format(
+                    thread_id,
+                    task
+                )
+            )  # Log the task
+
+            try:
+
+                # Call corresponding function
+                if task['type'] == 0:
+                    self.get_movies(
+                        thread_id,
+                        task['url']
+                    )
+                elif task['type'] == 1:
+                    self.download_movie(
+                        thread_id,
+                        task['url'],
+                        task['movie']
+                    )
+                elif task['type'] == 2:
+                    self.download_song(
+                        thread_id,
+                        task['url'],
+                        task['song'],
+                        task['movie'],
+                        task['movie_url']
+                    )
+
+                print_util.print_info(
+                    'Task complete : {0}'.format(
+                        task
+                    ),
+                    colors.GREEN
+                )  # Log success
+
+            except Exception as e:  # Some error
+                print_util.print_error(
+                    '{0} --> Error : {1}'.format(
+                        thread_id,
+                        e
+                    ),
+                    colors.RED
+                )  # Log it
+                task['n_errors'] += 1  # Increment number of errors
+                self.task_queue.put(task)  # Put back in queue
 
     def run(self):
-
         while True:
-
-            print('\n\n\n\n\n--------------------Starting New Crawl with {'
-                  '0}--------------------'.format(self.name))
-
-            self.task_queue = Queue()
-
+            # Crawl cycle start
+            print_util.print_info(
+                'Starting new crawl with {0}.'.format(
+                    self.name
+                )
+            )
+            # Add all URLs to task queue
             for url in self.url_list:
-                self.task_queue.put((0, url))
+                self.task_queue.put(
+                    {
+                        'type': 0,
+                        'url': url,
+                        'n_errors': 0  # No errors initially
+                    }
+                )
 
-            thread_dict = {}
+            # Start all threads
+            threads = []  # List for all threads
             for n in range(1, self.number_of_threads + 1):
-                temp_thread = Thread(target=self.threader, args=(n,))
-                thread_dict[n] = temp_thread
+                temp_thread = Thread(
+                    target=self.threader,  # Worker function
+                    args=(n,)  # Pass thread id as argument
+                )
+                threads.append(temp_thread)
                 temp_thread.start()
-            for n in range(1, self.number_of_threads + 1):
-                thread_dict[n].join()
+
+            # Wait for all tasks to finish, i.e. task_queue to get empty
+            # self.task_queue.join()
+
+            # Stop workers
+            # for n in range(1, self.number_of_threads + 1):
+            #     self.task_queue.put(None)
+            for temp_thread in threads:
+                temp_thread.join()
+
+                # Crawl cycle ends
 
     def download_movie(self, thread_id, url, movie):
 
-        print_util.print_info('{0} -> Downloading movie {1} -> {2}'.format(
-            thread_id, movie, url))
+        movie_website = self.start_url + url
+        raw_html = open_request(movie_website, delayed=self.delay_request)
 
-        if db_operations.is_old_movie(self.start_url, url):
-            print_util.print_info(
-                '{0} -> Skipping movie {1}, too old or too new.'.format(
+        song_with_url = self.get_songs_with_url(raw_html)
+
+        # No new songs added
+        if db_operations.number_of_songs(self.start_url, url) == len(
+                song_with_url):
+            db_operations.update_last_crawl(self.start_url, url)
+            print_util.print_warning(
+                '{0} --> Movie {1} contains no new songs. Skipping.'.format(
                     thread_id,
                     movie
                 )
             )
             return
 
-        movie_website = self.start_url + url
-        success, raw_html = open_request(thread_id, movie_website)
-
-        if not success:
-            self.task_queue.put((1, url, movie))
-            return
-
-        song_with_url = self.get_songs_with_url(raw_html)
-
-        if db_operations.number_of_songs(self.start_url, url) == len(
-                song_with_url):
-            db_operations.update_last_crawl(self.start_url, url)
-            print_util.print_info('{0} -> Skipping movie {1}, no new '
-                                  'songs.'.format(
-                thread_id,
-                movie
-            ))
-            return
-
+        # Add all songs
         for song_url, song in song_with_url:
-            song_html = ''
-            song_url_ = self.start_url + song_url
-            success, song_html = open_request(thread_id, song_url_)
-
-            if not success:
-                self.task_queue.put((1, url, movie))
-                return
-
-            lyrics, singers, music_by, lyricist = self.get_song_details(
-                song_html)
-
-            new_id = db_operations.save(
-                song=song,
-                song_url=song_url,
-                movie=movie,
-                movie_url=url,
-                start_url=self.start_url,
-                lyrics=lyrics,
-                singers=singers,
-                director=music_by,
-                lyricist=lyricist
+            self.task_queue.put(
+                {
+                    'type': 2,
+                    'url': song_url,
+                    'song': song,
+                    'movie': movie,
+                    'movie_url': url,
+                    'n_errors': 0
+                }
             )
 
-            print_util.print_info(
-                '{0} -> Saved details of song {1} ({2}) with id {3}'.format(
+    def download_song(self, thread_id, url, song, movie, movie_url):
+        # Song already exists
+        if db_operations.exists_song(self.start_url, url):
+            print_util.print_warning(
+                '{0} -> Song {1} already exists. Skipping.'.format(
                     thread_id,
-                    song,
-                    movie,
-                    new_id
+                    song
                 )
             )
+            return
+
+        # Get HTML
+        song_url_ = self.start_url + url
+        song_html = open_request(song_url_, delayed=self.delay_request)
+
+        lyrics, singers, music_by, lyricist = self.get_song_details(song_html)
+
+        # Save in database
+        db_operations.save(
+            song=song,
+            song_url=url,
+            movie=movie,
+            movie_url=movie_url,
+            start_url=self.start_url,
+            lyrics=lyrics,
+            singers=singers,
+            director=music_by,
+            lyricist=lyricist
+        )
 
     def get_movies(self, thread_id, url):
 
-        print_util.print_info('{0} -> Getting movies from page {1}.'.format(
-            thread_id, url))
-
+        # Get website HTML
         website = self.start_url + url
-        success, raw_html = open_request(thread_id, website)
+        raw_html = open_request(website, delayed=self.delay_request)
 
-        if not success:
-            self.task_queue.put((0, url))
-            return
-
+        # Add movies to task queue
         movies_with_url = self.get_movies_with_url(raw_html)
-
-        print_util.print_info('{0} -> Found {1} movies on {2}'.format(
-            thread_id,
-            len(movies_with_url),
-            url
-        ))
-
         for url, movie in movies_with_url:
-            self.task_queue.put((1, url, movie))
+            self.task_queue.put(
+                {
+                    'type': 1,
+                    'url': url,
+                    'movie': movie,
+                    'n_errors': 0
+                }
+            )
 
     def get_movies_with_url(self, raw_html):
         # User overrides this method to get list of movies from raw html
@@ -213,12 +296,21 @@ class CrawlerType1(BaseCrawler):
     def threader(self, thread_id):
 
         while not self.task_queue.empty():
-            task = self.task_queue.get()
+            try:
+                task = self.task_queue.get()
 
-            if task[0] == 0:
-                self.get_artists(thread_id, task[1])
-            elif task[0] == 1:
-                self.get_artist_albums(thread_id, task[1], task[2])
+                if task[0] == 0:
+                    self.get_artists(thread_id, task[1])
+                elif task[0] == 1:
+                    self.get_artist_albums(thread_id, task[1], task[2])
+            except Exception as e:
+                print_util.print_error(
+                    'Exception in thread {0} - {1}'.format(
+                        thread_id,
+                        e
+                    ),
+                    colors.RED
+                )
 
     def get_artists(self, thread_id, url):
         print_util.print_info(
@@ -345,32 +437,6 @@ class CrawlerType1(BaseCrawler):
         return 'la la la la'
 
 
-def open_request(thread_id, url, delayed=False, single_agent=False):
-    if not single_agent:
-        agent = get_header()
-    else:
-        agent = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/49.0.2623.108 Chrome/49.0.2623.108 Safari/537.36'}
-
-    req = request.Request(url, headers=agent)
-    if delayed:
-        sleep_for_some_time()
-    try:
-        response = request.urlopen(req)
-        raw_html = response.read().decode('utf-8', 'ignore')
-        response.close()
-    except Exception as e:
-        print_util.print_error(
-            '{0} -> Error downloading {1}. Exception : {2}. '.format(
-                thread_id,
-                url,
-                e
-            )
-        )
-        return False, ''
-    return True, raw_html
-
-
 class CrawlerType2(BaseCrawler):
     def __init__(self, name, start_url, list_of_urls, number_of_threads):
         super().__init__(name, start_url)
@@ -397,16 +463,25 @@ class CrawlerType2(BaseCrawler):
     def threader(self, thread_id):
         while not self.task_queue.empty():
 
-            task = self.task_queue.get()
+            try:
+                task = self.task_queue.get()
 
-            if task[0] == 0:
-                self.get_artists(thread_id, task[1])
-            elif task[0] == 1:
-                self.get_artist(thread_id, task[1], task[2])
-            elif task[0] == 2:
-                self.get_songs_from_page(thread_id, task[1], task[2])
-            elif task[0] == 3:
-                self.get_song(thread_id, task[1], task[2], task[3])
+                if task[0] == 0:
+                    self.get_artists(thread_id, task[1])
+                elif task[0] == 1:
+                    self.get_artist(thread_id, task[1], task[2])
+                elif task[0] == 2:
+                    self.get_songs_from_page(thread_id, task[1], task[2])
+                elif task[0] == 3:
+                    self.get_song(thread_id, task[1], task[2], task[3])
+            except Exception as e:
+                print_util.print_error(
+                    'Exception in thread {0} - {1}'.format(
+                        thread_id,
+                        e
+                    ),
+                    colors.RED
+                )
 
     def get_artists(self, thread_id, url):
 
@@ -474,7 +549,8 @@ class CrawlerType2(BaseCrawler):
                     '{0} -> Song {1} already exists. Skipping.'.format(
                         thread_id,
                         song
-                    )
+                    ),
+                    colors.ORANGE
                 )
 
         for page in pages[1:]:
@@ -501,7 +577,8 @@ class CrawlerType2(BaseCrawler):
             self.task_queue.put(
                 (
                     2,
-                    url
+                    url,
+                    artist
                 )
             )
             return
@@ -524,7 +601,8 @@ class CrawlerType2(BaseCrawler):
                     '{0} -> Song {1} allready exists. Skipping.'.format(
                         thread_id,
                         song
-                    )
+                    ),
+                    colors.ORANGE
                 )
 
     def get_song(self, thread_id, url, song, artist):
@@ -570,7 +648,8 @@ class CrawlerType2(BaseCrawler):
                 thread_id,
                 song,
                 new_id
-            )
+            ),
+            colors.GREEN
         )
 
     def get_song_details(self, raw_html):
@@ -610,6 +689,17 @@ class CrawlerType2(BaseCrawler):
 
 def get_header():
     return {'User-Agent': choice(headers)}
+
+
+def open_request(url, delayed=False):
+    req = request.Request(url, headers=get_header())
+    if delayed:
+        sleep_for_some_time()
+
+    response = request.urlopen(req)
+    raw_html = response.read().decode('utf-8', 'ignore')
+    response.close()
+    return raw_html
 
 
 def sleep_for_some_time():
--- crawler/print_util.py
+++ crawler/print_util.py
@@ -2,26 +2,33 @@ from datetime import datetime
 from time import time
 
 
+class colors:
+    RED = '\033[31m{0}\033[0m'
+    GREEN = '\033[32m{0}\033[0m'
+    ORANGE = '\033[33m{0}\033[0m'
+    WHITE = '\033[0m{0}\033[0m'
+
+
 def current_time():  # Get current time
-    return datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S.%f')
 
 
-def print_info(message):  # Information printing utility
-    pr(message, 'INF')
+def print_info(message, color=colors.WHITE):  # Information printing utility
+    pr(message, 'INF', color)
 
 
-def print_error(message):
-    pr(message, 'ERR')
+def print_error(message, color=colors.RED):
+    pr(message, 'ERR', color)
 
 
-def print_warning(message):
-    pr(message, 'WAR')
+def print_warning(message, color=colors.ORANGE):
+    pr(message, 'WAR', color)
 
 
-def print_usage(message):
-    pr(message, 'USG')
+def print_usage(message, color=colors.WHITE):
+    pr(message, 'USG', color)
 
 
-def pr(m1, m2):
+def pr(m1, m2, color):
     message = '(' + current_time() + ') ' + m2 + ': ' + m1
-    print(message)
+    print(color.format(message))
