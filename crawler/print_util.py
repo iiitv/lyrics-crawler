204d64e0f4212fe8a0a7bf4e2f50ad2dda82fa1a
Pratyush Singh <singh.pratyush96@gmail.com>
2016-06-20 23:26:37 +0530

    Added black color to in Colors class

--- crawler/print_util.py
+++ crawler/print_util.py
@@ -2,30 +2,31 @@ from datetime import datetime
 from time import time
 
 
-class colors:
+class Colors:
     RED = '\033[31m{0}\033[0m'
     GREEN = '\033[32m{0}\033[0m'
     ORANGE = '\033[33m{0}\033[0m'
     WHITE = '\033[0m{0}\033[0m'
+    BLACK = '\033[30m{0}\033[0m'
 
 
 def current_time():  # Get current time
     return datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S.%f')
 
 
-def print_info(message, color=colors.WHITE):  # Information printing utility
+def print_info(message, color=Colors.WHITE):  # Information printing utility
     pr(message, 'INF', color)
 
 
-def print_error(message, color=colors.RED):
+def print_error(message, color=Colors.RED):
     pr(message, 'ERR', color)
 
 
-def print_warning(message, color=colors.ORANGE):
+def print_warning(message, color=Colors.ORANGE):
     pr(message, 'WAR', color)
 
 
-def print_usage(message, color=colors.WHITE):
+def print_usage(message, color=Colors.WHITE):
     pr(message, 'USG', color)
 
 
